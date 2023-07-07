b16573b590dd5d75afbfd68de731e5eecf31a45a
Emilly Zaytseva <emillyzaytseva@gmail.com>
2023-07-07 23:10:17 +0100

    0101

--- src/test/java/base_urls/GmiBankBaseUrl.java
+++ src/test/java/base_urls/GmiBankBaseUrl.java
@@ -5,6 +5,8 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
+import static utils.AuthenticationHerOkuApp.generateToken;
+
 public class GmiBankBaseUrl {
     // This class is created to prevent repeated action in request like Base URL,
     // contain type,authorization etc.
@@ -14,6 +16,8 @@ public class GmiBankBaseUrl {
     public void setUp(){
 
         spec= new RequestSpecBuilder().
+                addHeader("Authorization","Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXJrX3R3YWluIiwiYXV0aCI6IlJPTEVfQURNSU4sUk9MRV9NQU5BR0VSIiwiZXhwIjoxNjg4ODI2ODk1fQ." +
+                        "Ud1h-pDjuEs7JHm1rine_OJQaQrdVSMGE327rjULkymCwTX4wSbDaqRHUEl899MQPy1RdoBiF39aYzDwr-qI3w").
                 setContentType(ContentType.JSON).
                 setBaseUri("https://www.gmibank.com/").
                 build();
